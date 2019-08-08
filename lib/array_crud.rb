3977e3d776e845f6b67be9304dd57f0019eae9d3
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 05:47:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,31 +1,31 @@
 def create_an_empty_array
   []
-  
+
 end
 
 def create_an_array
   my_array=["pineapple", "coconut"]
-  
+
 end
 
 def add_element_to_end_of_array(array, element)
   my_array << "mango"
-  
+
 end
 
 def add_element_to_start_of_array(array, element)
   my_array.unshift("papaya")
-  
+
 end
 
 def remove_element_from_end_of_array(array)
   coconut = my_array.pop
-  
+
 end
 
 def remove_element_from_start_of_array(array)
   mango = my_array.shift
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
